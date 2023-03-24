9999a399a29b3f9ab2919dac0cfd82862bb85673
Alex <aleksandrkvitko567@gmail.com>
2023-03-24 16:32:53 +0200

    Створення папки для виконання завдання по Lesson 11

---
+++ dz_lesson_11/main_1.py
@@ -0,0 +1,13 @@
+'''
+Practice
+
+1.  Create a program that will ask user to search a word. 
+    Search this word in Giphy (use their API). 
+    Return links to these GIFs as a result
+
+2.  Взяти API-weather, розпарсити і вивезти погоду від користувача(вводить локацію, 
+    по цій локації повернеться погода, вологість і тд) https://openweathermap.org 
+    *Потрібна реєстрація і створення свого api-ключа
+
+3.  Вивезти всіх космонавтів(кількість і імена) http://api.open-notify.org/astros.json
+'''
