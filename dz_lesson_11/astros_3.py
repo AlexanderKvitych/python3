8d208fc6c6d427f1fee6339833d80b3961b309ad
Alex <aleksandrkvitko567@gmail.com>
2023-04-04 10:14:33 +0300

    Домашнє завдання API погода

---
+++ dz_lesson_11/astros_3.py
@@ -0,0 +1,11 @@
+# Вивезти всіх космонавтів(кількість і імена) http://api.open-notify.org/astros.json
+
+import requests
+my_list = []
+astros = requests.get('http://api.open-notify.org/astros.json')
+for obj in astros.json()['people']:
+    # print(obj)
+    if obj in astros.json()['people'] and obj not in my_list:
+        my_list.append(obj['name'])
+        # print(my_list, len(my_list))
+print(f'Список космонавтів: {my_list}\nКількість космонавтів: {len(my_list)}')
